1f3e6eb073c910827262441575fe25a250801c9b
marissa-mayes <marissa.mayes1289@gmail.com>
2020-09-17 01:16:35 -0500

    server update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 //const host = "0.0.0.0";
-const PORT = process.env.PORT || 30067;
-
+const PORT = process.env.PORT || 30068;
+const url = 'mongodb://127.0.0.1:27017'
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 mongoose.connect(
- process.env.MONGODB_URI || "mongodb://localhost/transactions",{
+ process.env.MONGODB_URI || "mongodb://localhost/budget",{
    useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
