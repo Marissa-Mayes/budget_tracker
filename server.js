0cc557af73aa3c1defdcd798ab7e5999454dfc9e
marissa-mayes <marissa.mayes1289@gmail.com>
2020-09-16 21:58:04 -0500

    updated connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 //const host = "0.0.0.0";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30067;
 
 const app = express();
 
@@ -14,14 +14,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budgettracker",{
+ process.env.MONGODB_URI || "mongodb://localhost/budgettracker",{
    useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
   useCreateIndex: true
-});
+}).catch(err=>console.log("CONNECTION ERROR: ",err))
 
 // routes
 app.use(require("./routes/api.js"));
